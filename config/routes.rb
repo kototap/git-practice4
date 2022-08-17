df2f8d92183b4f89524ef252f665fd1c52662db9
Kototap <106796386+kototap@users.noreply.github.com>
2022-08-17 14:45:07 +0000

    memories/controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'memories/new'
+  get 'memories/show'
+  get 'memories/index'
   get 'homes/top'
   get 'homes/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
